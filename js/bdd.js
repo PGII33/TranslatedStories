9a1ceeee1aae72826a8867044dc6df2e0c915fdd
Micky <micky.mari@etu.u-bordeaux.fr>
2024-06-04 00:32:38 +0200

    Crea bdd.js : - bdd temporaire

---
+++ js/bdd.js
@@ -0,0 +1,10 @@
+var stories = [
+    {
+        "title": "Paranoic Mage",
+        "default_language": "english",
+        "content": [
+            { "english": "There is actually nothing"},
+            { "french": "Il n'y a en fait rien"}
+        ]
+    }
+];
