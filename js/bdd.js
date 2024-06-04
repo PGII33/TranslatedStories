ab49765ec8a20f07c359c3156b4fb4edbce1adb3
Micky <micky.mari@etu.u-bordeaux.fr>
2024-06-04 10:15:31 +0200

    updt bdd.js : - Changement de la forme

--- js/bdd.js
+++ js/bdd.js
@@ -1,10 +1,9 @@
 var stories = [
     {
         "title": "Paranoic Mage",
-        "default_language": "english",
-        "content": [
-            { "english": "There is actually nothing"},
-            { "french": "Il n'y a en fait rien"}
-        ]
+        "list_of_languages": ["english", "french"],
+        "original_content": "english",
+        "english": "There is actually nothing",
+        "french": "Il n'y a en fait rien"
     }
 ];
